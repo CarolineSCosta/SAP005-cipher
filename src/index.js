0854a183a0e8e906a75f57fae6c14e36d64394c7
Caroline Costa <carolmarcionilia@gmail.com>
2020-10-27 20:04:55 -0300

    Começando a integrar com JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let offsetKey = window.document.querySelector('#offsetKey')
+let textBox = window.document.querySelector('#messageTextArea')
+let cipherButton = window.document.querySelector('#cipherButton')
+let decipherButton = window.document.querySelector('#decipherButton')
+let finalMsg = window.document.querySelector('#finalMsg')
+
+
+function cipher() {
+
+}
+
+function decipher() {
+
+}
