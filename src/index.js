528fdce85e0eb05cb5acc4d4c862a9c1c673e2ec
Caroline Costa <carolmarcionilia@gmail.com>
2020-10-27 23:44:01 -0300

    Printando texto na tela

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 let offsetKey = window.document.querySelector('#offsetKey')
-let textBox = window.document.querySelector('#messageTextArea')
 let cipherButton = window.document.querySelector('#cipherButton')
+let textBox = window.document.querySelector('#messageTextArea')
 let decipherButton = window.document.querySelector('#decipherButton')
 let finalMsg = window.document.querySelector('#finalMsg')
 
+cipherButton.addEventListener('click', cipherClick)
 
-function cipher() {
-
-}
-
-function decipher() {
-
+function cipherClick() {
+    let originalMessage = textBox.value
+    finalMsg.innerHTML = originalMessage
 }
