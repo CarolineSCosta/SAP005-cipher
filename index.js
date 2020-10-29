8677e1c019e859a66180a936b8597d6f2cfa5e38
Caroline Costa <carolmarcionilia@gmail.com>
2020-10-28 23:50:21 -0300

    Desenvolvendo a lógica da cifra

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-let offsetKey = window.document.querySelector('#offsetKey')
-let cipherButton = window.document.querySelector('#cipherButton')
-let textBox = window.document.querySelector('#messageTextArea')
+let textArea = window.document.querySelector('#messageTextArea')
+let numberInput = window.document.querySelector('#offsetKey')
 let decipherButton = window.document.querySelector('#decipherButton')
-let finalMsg = window.document.querySelector('#finalMsg')
+let cipherButton = window.document.querySelector('#cipherButton')
+let resultMsg = window.document.querySelector('#finalMsg')
 
 cipherButton.addEventListener('click', cipherClick)
 
 function cipherClick() {
-    let originalMessage = textBox.value
-    finalMsg.innerHTML = originalMessage
+    let originalMessage = textArea.value
+    let offset = Number(numberInput.value)
+    let messageCharCode = Number(originalMessage.charCodeAt())
+    let charDecode = ((messageCharCode - 65 + offset) % 26) + 65
+    let cipherMessage = String.fromCharCode(charDecode)
+    resultMsg.innerHTML = `A sua mensagem é ${cipherMessage}`
 }
