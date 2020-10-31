e1ad3eb6a2cae8b933efc0e016cdac7907616153
Caroline Costa <carolmarcionilia@gmail.com>
2020-10-31 18:49:19 -0300

    Bug Decode

--- src/index.js
+++ src/index.js
@@ -2,22 +2,29 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-let textArea = window.document.querySelector('#messageTextArea')
-let numberInput = window.document.querySelector('#offsetKey')
-let decipherButton = window.document.querySelector('#decipherButton')
-let cipherButton = window.document.querySelector('#cipherButton')
-let resultMsg = window.document.querySelector('#finalMsg')
+const textArea = window.document.querySelector('#messageTextArea')
+const numberInput = window.document.querySelector('#offsetKey')
+const decipherButton = window.document.querySelector('#decipherButton')
+const cipherButton = window.document.querySelector('#cipherButton')
+const clearButton = window.document.querySelector("#clearButton")
+const resultMsg = window.document.querySelector('#finalMsg')
 
 cipherButton.addEventListener('click', () => {
-    let originalMessage = textArea.value
-    let offset = Number(numberInput.value)
-    let cipherMessage = cipher.encode(offset, originalMessage)
-    resultMsg.innerHTML = cipherMessage
+    const originalMessage = textArea.value
+    const offset = Number(numberInput.value)
+    const cipherMessage = cipher.encode(offset, originalMessage)
+    resultMsg.value = cipherMessage
 })
 
 decipherButton.addEventListener('click', () => {
-    let originalMessage = textArea.value
-    let offset = Number(numberInput.value)
-    let decipherMessage = cipher.decode(offset, originalMessage)
-    resultMsg.innerHTML = decipherMessage
+    const originalMessage = textArea.value
+    const offset = Number(numberInput.value)
+    const decipherMessage = cipher.decode(offset, originalMessage)
+    resultMsg.value = decipherMessage
+})
+
+clearButton.addEventListener("click", () => {
+    textArea.value = ""
+    numberInput.value = ""
+    resultMsg.value = ""
 })
