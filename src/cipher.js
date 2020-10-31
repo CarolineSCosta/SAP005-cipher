c370429e0cd5cea4e220b8f814aea99692e7663b
Caroline Costa <carolmarcionilia@gmail.com>
2020-10-31 16:33:19 -0300

    Codificando corretamente

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,35 @@
 const cipher = {
 
     encode: function(offset, string) {
-        const firstLetterAscii = 65
-        const alphabetLenght = 25
-        let messageCharCode = Number(string.toUpperCase().charCodeAt())
-        let charDecode = ((messageCharCode - firstLetterAscii + offset) % alphabetLenght) + firstLetterAscii
-        let cipherMessage = String.fromCharCode(charDecode)
+        const firstAsciiUpper = 65
+        const firstAsciiLower = 97
+        const alphabetLength = 25
+        let cipherMessage = ""
+
+
+        for (let i = 0; i < string.length; i++) {
+            let messageCharCode = Number(string[i].charCodeAt())
+            let charDecode = ""
+            if ((messageCharCode >= 65) && (messageCharCode <= 90)) {
+                charDecode = ((messageCharCode - firstAsciiUpper + offset) % alphabetLength) + firstAsciiUpper
+            } else if ((messageCharCode >= 97) && (messageCharCode <= 122)) {
+                charDecode = ((messageCharCode - firstAsciiLower + offset) % alphabetLength) + firstAsciiLower
+            } else {
+                charDecode = messageCharCode
+            }
+            cipherMessage = cipherMessage + String.fromCharCode(charDecode)
+        }
+
         return cipherMessage
     },
 
     decode: function(offset, string) {
         const firstLetterAscii = 65
-        const alphabetLenght = 25
+        const alphabetLength = 25
         let messageCharCode = Number(string.toUpperCase().charCodeAt())
-        let charDecode = ((messageCharCode - firstLetterAscii - offset) % alphabetLenght) + firstLetterAscii
+        let charDecode = ((messageCharCode - firstLetterAscii - offset) % alphabetLength) + firstLetterAscii
         let cipherMessage = String.fromCharCode(charDecode)
+
         return cipherMessage
 
     }
