d9cb3cb1dc5b54a83b8ef9d2ae14da06e375b6fb
Caroline Costa <carolmarcionilia@gmail.com>
2020-10-31 18:49:19 -0300

    Bug Decode

--- cipher.js
+++ cipher.js
@@ -3,12 +3,12 @@ const cipher = {
     encode: function(offset, string) {
         const firstAsciiUpper = 65
         const firstAsciiLower = 97
-        const alphabetLength = 25
+        const alphabetLength = 26
         let cipherMessage = ""
 
 
         for (let i = 0; i < string.length; i++) {
-            let messageCharCode = Number(string[i].charCodeAt())
+            const messageCharCode = Number(string[i].charCodeAt())
             let charDecode = ""
             if ((messageCharCode >= 65) && (messageCharCode <= 90)) {
                 charDecode = ((messageCharCode - firstAsciiUpper + offset) % alphabetLength) + firstAsciiUpper
@@ -24,15 +24,27 @@ const cipher = {
     },
 
     decode: function(offset, string) {
-        const firstLetterAscii = 65
-        const alphabetLength = 25
-        let messageCharCode = Number(string.toUpperCase().charCodeAt())
-        let charDecode = ((messageCharCode - firstLetterAscii - offset) % alphabetLength) + firstLetterAscii
-        let cipherMessage = String.fromCharCode(charDecode)
+        const firstAsciiUpper = 65
+        const firstAsciiLower = 97
+        const alphabetLength = 26
+        let cipherMessage = ""
+
+
+        for (let i = 0; i < string.length; i++) {
+            const messageCharCode = Number(string[i].charCodeAt())
+            let charDecode = ""
+            if ((messageCharCode >= 65) && (messageCharCode <= 90)) {
+                charDecode = ((messageCharCode - firstAsciiUpper - offset) % alphabetLength) + firstAsciiUpper
+            } else if ((messageCharCode >= 97) && (messageCharCode <= 122)) {
+                charDecode = ((messageCharCode - firstAsciiLower - offset) % alphabetLength) + firstAsciiLower
+            } else {
+                charDecode = messageCharCode
+            }
+            cipherMessage = cipherMessage + String.fromCharCode(charDecode)
+        }
 
         return cipherMessage
-
-    }
+    },
 };
 
 export default cipher;
